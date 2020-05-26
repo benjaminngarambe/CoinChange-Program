c77de159925cdc23cc525c5c24d457bac40ca68b
BENJAMIN NGARAMBE <kukibyanze@gmail.com>
2020-05-26 22:39:53 +0200

    final

--- Program.cs
+++ Program.cs
@@ -99,72 +99,76 @@ namespace project2._1
 
             //if he chooses to do it automatically
 
-            if (AnswerOne == 1)
+            switch (AnswerOne)
             {
-                int[] Coins = { 1, 2, 5, 10, 20, 50, 100 };
-                Console.WriteLine("----------------------------------------------------");
-                //prompting the user to enter the value to be changed
-                Console.WriteLine("Enter value for change");
-                change = Convert.ToInt32(Console.ReadLine());
+                case 1:
+                    int[] Coins = { 1, 2, 5, 10, 20, 50, 100 };
+                    Console.WriteLine("----------------------------------------------------");
+                    //prompting the user to enter the value to be changed
+                    Console.WriteLine("Enter value for change");
+                    change = Convert.ToInt32(Console.ReadLine());
 
-                //if statement to make sure that rules for being give change are applied
-                if (change < 0)
-                {
-                    Console.WriteLine("Input should be a positive integer");
-                }
-                else if (change == 0)
-                {
-                    Console.WriteLine("empty string");
-                }
-                Console.WriteLine("-------------------------------");
-                //implementing the make greedy change function
-                makeGreedyChange(Coins, change);
-                makeGreedyChangeGeneral(Coins, change);
-            }
+                    //if statement to make sure that rules for being give change are applied
+                    if (change < 0)
+                    {
+                        Console.WriteLine("Input should be a positive integer");
+                    }
+                    else if (change == 0)
+                    {
+                        Console.WriteLine("empty string");
+                    }
+                    Console.WriteLine("-------------------------------");
+                    //implementing the make greedy change function
+                    makeGreedyChange(Coins, change);
+                    makeGreedyChangeGeneral(Coins, change);
+                    break;
 
-            //if he chooses to enter it by himself
-            else if (AnswerOne == 2)
-            {
-                Console.WriteLine("----------------------------------------------------");
-                Console.WriteLine("You have chosen to enter the number available for making changes");
-                //to prompt the user to enter the number of elements that are going to be needed for him to make a change
-                Console.WriteLine("Enter the Number of changes that are going to compose the array");
-                int z = Convert.ToInt32(Console.ReadLine());
+                //if he chooses to enter it by himself
+                case 2:
 
-                //the string that is going to save the user array of changes
-                int[] answerTwo = new int[z];
-                for (int i = 0; i < answerTwo.Length; i++)
-                {
-                    answerTwo[i] = Convert.ToInt32(Console.ReadLine());
-                }
-                Console.WriteLine("---------------------------");
-                //display the array the user entered
-                Console.WriteLine("this is the array you entered: ");
-                Console.WriteLine("[{0}]", string.Join(", ", answerTwo));
-                Console.WriteLine("---------------------------");
-                //the code to make the change that was previously used in the AnswerOne == 1 statement
-                //prompting the user to enter the value to be changed
-                Console.WriteLine("Enter value for change");
-                change = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("----------------------------------------------------");
+                    Console.WriteLine("You have chosen to enter the number available for making changes");
+                    //to prompt the user to enter the number of elements that are going to be needed for him to make a change
+                    Console.WriteLine("Enter the Number of changes that are going to compose the array");
+                    int z = Convert.ToInt32(Console.ReadLine());
 
-                //if statement to make sure that rules for being give change are applied
-                if (change < 0)
-                {
-                    Console.WriteLine("!!! Input should be a positive integer !!!");
-                }
-                else if (change == 0)
-                {
-                    Console.WriteLine("empty string");
-                }
-                Console.WriteLine("-------------------------------");
-                //implementing the make greedy change function
-                makeGreedyChange(answerTwo, change);
-                makeGreedyChangeGeneral(answerTwo, change);
-            }
-            else
-            {
-                Console.WriteLine("!!! you have entered the wrong variables !!!");
-                Console.WriteLine("!!! Try again...and try to enter the right variables !!!");
+                    //the string that is going to save the user array of changes
+                    int[] answerTwo = new int[z];
+                    for (int i = 0; i < answerTwo.Length; i++)
+                    {
+                        answerTwo[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    Console.WriteLine("---------------------------");
+                    //display the array the user entered
+                    Console.WriteLine("this is the array you entered: ");
+                    Console.WriteLine("[{0}]", string.Join(", ", answerTwo));
+                    Console.WriteLine("---------------------------");
+                    //the code to make the change that was previously used in the AnswerOne == 1 statement
+                    //prompting the user to enter the value to be changed
+                    Console.WriteLine("Enter value for change");
+                    change = Convert.ToInt32(Console.ReadLine());
+
+                    //if statement to make sure that rules for being give change are applied
+                    if (change < 0)
+                    {
+                        Console.WriteLine("!!! Input should be a positive integer !!!");
+                    }
+                    else if (change == 0)
+                    {
+                        Console.WriteLine("empty string");
+                    }
+                    Console.WriteLine("-------------------------------");
+                    //implementing the make greedy change function
+                    makeGreedyChange(answerTwo, change);
+                    makeGreedyChangeGeneral(answerTwo, change);
+                    break;
+
+                default:
+                    Console.Clear();
+                    Console.WriteLine("!!! you have entered the wrong variables !!!");
+                    Console.WriteLine("!!! Try again...and try to enter the right variables !!!");
+                    Main(null);
+                    break;
             }
         }
     }
